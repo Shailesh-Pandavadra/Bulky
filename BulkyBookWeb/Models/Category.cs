9275c49f2109a9da7730c498532243fcd6f65685
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-03 17:42:51 +0530

    Section 3 - Display all Categories

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,7 +9,9 @@ namespace BulkyBookWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
