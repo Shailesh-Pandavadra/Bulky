0accdb8f96284008d4a187e739098a2630ddbf5b
Shailesh Pandavadra <pandavadra.shailesh@outlook.com>
2025-12-04 12:49:20 +0530

    Section 3 - Custom Validation

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -29,6 +29,12 @@ namespace BulkyBookWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+                //ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
